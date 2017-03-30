b14a660d1a8f4ca1c48235818487d46a7ad18b4d
Oscar Casamitjana <oscar.casamitjana@globalzitro.com>
2017-03-30 12:28:00 +0200

    Added a FPS counter

---
+++ fpstext.cpp
@@ -0,0 +1,49 @@
+#include "fpstext.h"
+#include <QDateTime>
+#include <QBrush>
+#include <QPainter>
+
+
+
+FPSText::FPSText(QQuickItem *parent): QQuickPaintedItem(parent), _currentFPS(0), _cacheCount(0)
+{
+    _times.clear();
+    setFlag(QQuickItem::ItemHasContents);
+}
+
+FPSText::~FPSText()
+{
+}
+
+void FPSText::recalculateFPS()
+{
+    qint64 currentTime = QDateTime::currentDateTime().toMSecsSinceEpoch();
+    _times.push_back(currentTime);
+
+    while (_times[0] < currentTime - 1000) {
+        _times.pop_front();
+    }
+
+    int currentCount = _times.length();
+    _currentFPS = (currentCount + _cacheCount) / 2;
+    if (currentCount != _cacheCount) fpsChanged(_currentFPS);
+    _cacheCount = currentCount;
+}
+
+int FPSText::fps()const
+{
+    return _currentFPS;
+}
+
+void FPSText::paint(QPainter *painter)
+{
+    recalculateFPS();
+    //qDebug() << __FUNCTION__;
+    QBrush brush(Qt::transparent);
+
+    painter->setBrush(brush);
+    painter->setPen(Qt::NoPen);
+    painter->setRenderHint(QPainter::Antialiasing);
+    painter->drawRoundedRect(0, 0, boundingRect().width(), boundingRect().height(), 0, 0);
+    update();
+}
